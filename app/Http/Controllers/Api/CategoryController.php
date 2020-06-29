2c4e2785f94224d91587a40efdc5fa46ea91207e
Composera <asadxoja5651@gmail.com>
2020-06-29 22:25:32 +0500

    added createCupboard method in category controller

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function createCupboard(Request $request)
+    {
+        return response()->json([
+            'all' => $request->all()
+        ]);
+    }
 }
